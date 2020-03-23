a0204686d2480af9113e49638a4366003ef876b4
Jaroslaw <jaroslaw.konczak@gmail.com>
2020-03-23 18:55:16 +0100

    initial commit

---
+++ src/main/java/io/github/cadiboo/examplemod/client/ClientModEventSubscriber.java
@@ -0,0 +1,56 @@
+package io.github.cadiboo.examplemod.client;
+
+import io.github.cadiboo.examplemod.ExampleMod;
+import io.github.cadiboo.examplemod.client.gui.ElectricFurnaceScreen;
+import io.github.cadiboo.examplemod.client.gui.HeatCollectorScreen;
+import io.github.cadiboo.examplemod.client.gui.ModFurnaceScreen;
+import io.github.cadiboo.examplemod.client.render.tileentity.ElectricFurnaceTileEntityRenderer;
+import io.github.cadiboo.examplemod.client.render.tileentity.MiniModelTileEntityRenderer;
+import io.github.cadiboo.examplemod.init.ModContainerTypes;
+import io.github.cadiboo.examplemod.tileentity.ElectricFurnaceTileEntity;
+import io.github.cadiboo.examplemod.tileentity.MiniModelTileEntity;
+import net.minecraft.client.gui.ScreenManager;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Subscribe to events from the MOD EventBus that should be handled on the PHYSICAL CLIENT side in this class
+ *
+ * @author Cadiboo
+ */
+@EventBusSubscriber(modid = ExampleMod.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public final class ClientModEventSubscriber {
+
+	private static final Logger LOGGER = LogManager.getLogger(ExampleMod.MODID + " Client Mod Event Subscriber");
+
+	/**
+	 * This method will be called by Forge when it is time for the mod to do its client-side setup
+	 * This method will always be called after the Registry events.
+	 * This means that all Blocks, Items, TileEntityTypes, etc. will all have been registered already
+	 */
+	@SubscribeEvent
+	public static void onFMLClientSetupEvent(final FMLClientSetupEvent event) {
+
+		// Register TileEntity Renderers
+		ClientRegistry.bindTileEntitySpecialRenderer(MiniModelTileEntity.class, new MiniModelTileEntityRenderer());
+		ClientRegistry.bindTileEntitySpecialRenderer(ElectricFurnaceTileEntity.class, new ElectricFurnaceTileEntityRenderer());
+		LOGGER.debug("Registered TileEntity Renderers");
+
+		// Register Entity Renderers
+//		RenderingRegistry.registerEntityRenderingHandler(YourEntity.class, YourEntityRenderer::new);
+//		LOGGER.debug("Registered Entity Renderers");
+
+		// Register ContainerType Screens
+		ScreenManager.registerFactory(ModContainerTypes.HEAT_COLLECTOR, HeatCollectorScreen::new);
+		ScreenManager.registerFactory(ModContainerTypes.ELECTRIC_FURNACE, ElectricFurnaceScreen::new);
+		ScreenManager.registerFactory(ModContainerTypes.MOD_FURNACE, ModFurnaceScreen::new);
+		LOGGER.debug("Registered ContainerType Screens");
+
+	}
+
+}
---
+++ src/main/java/io/github/cadiboo/examplemod/client/render/tileentity/MiniModelTileEntityRenderer.java
@@ -0,0 +1,143 @@
+package io.github.cadiboo.examplemod.client.render.tileentity;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import io.github.cadiboo.examplemod.client.render.MiniModel;
+import io.github.cadiboo.examplemod.config.ExampleModConfig;
+import io.github.cadiboo.examplemod.tileentity.MiniModelTileEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.RegionRenderCacheBuilder;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.chunk.CompiledChunk;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.client.renderer.vertex.VertexFormatElement;
+import org.lwjgl.opengl.GL11;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import static io.github.cadiboo.examplemod.client.render.MiniModel.BLOCK_RENDER_LAYERS;
+
+/**
+ * Renders a model of the surrounding blocks.
+ * This should really probably not be in an examplemod for beginners,
+ * but I added comments to it so its all good
+ *
+ * @author Cadiboo
+ */
+public class MiniModelTileEntityRenderer extends TileEntityRenderer<MiniModelTileEntity> {
+
+	/**
+	 * Render our TileEntity
+	 */
+	@Override
+	public void render(final MiniModelTileEntity tileEntityIn, final double x, final double y, final double z, final float partialTicks, final int destroyStage) {
+		super.render(tileEntityIn, x, y, z, partialTicks, destroyStage);
+
+		final MiniModel miniModel = tileEntityIn.miniModel;
+
+		if (miniModel == null)
+			return;
+
+		if (!miniModel.isBuilt())
+			miniModel.rebuild();
+
+		// Setup correct GL state
+		this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+		RenderHelper.disableStandardItemLighting();
+		// Translucency
+		if (ExampleModConfig.modelTranslucency) {
+			GlStateManager.blendFunc(GL11.GL_ONE, GL11.GL_ONE);
+		} else {
+			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		}
+		GlStateManager.enableBlend();
+
+		if (Minecraft.isAmbientOcclusionEnabled()) {
+			GlStateManager.shadeModel(GL11.GL_SMOOTH);
+		} else {
+			GlStateManager.shadeModel(GL11.GL_FLAT);
+		}
+
+		GlStateManager.pushMatrix();
+
+		// Translate to render pos. The 0.5 is to translate into the centre of the block, rather than to the corner of it
+		GlStateManager.translated(x + 0.5, y + 0.5, z + 0.5);
+
+		final double scale = ExampleModConfig.modelScale;
+		GlStateManager.scaled(scale, scale, scale);
+
+		// Translate to start of render (our TileEntity is at its centre)
+		GlStateManager.translated(-8, -8, -8);
+
+		// Render the buffers
+		renderChunkBuffers(miniModel.regionRenderCacheBuilder, miniModel.generator.getCompiledChunk());
+
+		GlStateManager.popMatrix();
+
+		// Clean up GL state
+		RenderHelper.enableStandardItemLighting();
+
+	}
+
+	/**
+	 * This renderer is a global renderer.
+	 * This means that it will always render, even if the player is not able to see it's block.
+	 * This is useful for rendering larger models or dynamically sized models.
+	 * The Beacon's beam is also a global renderer
+	 */
+	@Override
+	public boolean isGlobalRenderer(final MiniModelTileEntity te) {
+		return true;
+	}
+
+	/**
+	 * Loops through every non-empty {@link BufferBuilder} in buffers and renders the buffer without resetting it
+	 *
+	 * @param buffers       The {@link RegionRenderCacheBuilder} to get {@link BufferBuilder}s from
+	 * @param compiledChunk The {@link CompiledChunk} to use to check if a layer has any rendered blocks
+	 */
+	private void renderChunkBuffers(final RegionRenderCacheBuilder buffers, final CompiledChunk compiledChunk) {
+		final int length = BLOCK_RENDER_LAYERS.length;
+		// Render each buffer that has been used
+		for (int layerOrdinal = 0; layerOrdinal < length; ++layerOrdinal) {
+			if (!compiledChunk.isLayerEmpty(BLOCK_RENDER_LAYERS[layerOrdinal])) {
+				drawBufferWithoutResetting(buffers.getBuilder(layerOrdinal));
+			}
+		}
+	}
+
+	/**
+	 * Copy of net.minecraft.client.renderer.WorldVertexBufferUploader#draw(net.minecraft.client.renderer.BufferBuilder)
+	 * The only difference is that it does NOT reset the buffer after drawing it
+	 *
+	 * @param bufferBuilderIn the buffer builder to draw
+	 */
+	private void drawBufferWithoutResetting(final BufferBuilder bufferBuilderIn) {
+		if (bufferBuilderIn.getVertexCount() > 0) {
+			VertexFormat vertexformat = bufferBuilderIn.getVertexFormat();
+			int i = vertexformat.getSize();
+			ByteBuffer bytebuffer = bufferBuilderIn.getByteBuffer();
+			List<VertexFormatElement> list = vertexformat.getElements();
+
+			for (int j = 0; j < list.size(); ++j) {
+				VertexFormatElement vertexformatelement = list.get(j);
+				vertexformatelement.getUsage().preDraw(vertexformat, j, i, bytebuffer); // moved to VertexFormatElement.preDraw
+			}
+
+			GlStateManager.drawArrays(bufferBuilderIn.getDrawMode(), 0, bufferBuilderIn.getVertexCount());
+			int i1 = 0;
+
+			for (int j1 = list.size(); i1 < j1; ++i1) {
+				VertexFormatElement vertexformatelement1 = list.get(i1);
+				vertexformatelement1.getUsage().postDraw(vertexformat, i1, i, bytebuffer); // moved to VertexFormatElement.postDraw
+			}
+		}
+
+		// Commented out - don't reset the buffer
+//		bufferBuilderIn.reset();
+	}
+
+}
